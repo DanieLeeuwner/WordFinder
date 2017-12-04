03f73f7d8e58665c27b16f27a1bcab9ff84742c9
Danie Leeuwner <dvl@waleeuwner.co.za>
2017-12-04 09:12:52 +0200

    Improved search to use regex

--- WordFinder/Program.cs
+++ WordFinder/Program.cs
@@ -2,89 +2,131 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace WordFinder
 {
-    class Program
+  internal class Program
+  {
+    private static string word_file = "words.txt";
+    private static string dictionary_file = "dictionary.txt";
+
+    private static List<string> haystack;
+
+    private static string selected_word;
+
+    private static void Main(string[] args)
     {
-        static string word_file = "words.txt";
+      if (!File.Exists(word_file))
+      {
+        Console.Write("Word list not found");
+        return;
+      }
 
+      Console.Write("Search: ");
 
-        static List<string> haystack;        
+      string needle = "";
 
-        static void Main(string[] args)
+      haystack = File.ReadAllLines(word_file).ToList();
+
+      while (true)
+      {
+        var input = Console.ReadKey();
+
+        if (input.Key == ConsoleKey.Backspace)
         {
-            if (!File.Exists(word_file))
-            {
-                Console.Write("Word list not found");
-                return;
-            }
-
-            Console.Write("Search: ");
-
-            string needle = "";
-
-            haystack = File.ReadAllLines(word_file).ToList();
-
-            while (true)
-            {
-                var input = Console.ReadKey();
-
-                if (input.Key == ConsoleKey.Backspace)
-                {
-                    if (needle.Length > 0)
-                    {
-                        needle = needle.Substring(0, needle.Length - 1);
-                    }
-                }
-                else
-                {
-                    needle += input.KeyChar;
-                }
-
-                var filtered_words = haystack.Where(hw => hw.Contains(needle)).Take(25);
-
-                Console.Clear();
-
-                Console.WriteLine($"Search: {needle}");
-                Console.WriteLine("-----------------------------------");
-
-                if (filtered_words.Count() == 0)
-                {
-                    var prevColor = Console.ForegroundColor;
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("--- No Words Found ---");
-
-                    Console.ForegroundColor = prevColor;
-                }
-
-                foreach (string fw in filtered_words)
-                {
-                    int start_index = fw.IndexOf(needle);
-
-                    string before = fw.Substring(0, start_index);
-
-                    string after = fw.Substring(start_index + needle.Length);
-
-                    Console.Write(before);
-
-                    var prevColor = Console.ForegroundColor;
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-
-                    Console.Write(needle);
-
-                    Console.ForegroundColor = prevColor;
-                    Console.Write(after + "\n");
-
-                }
-
-            }
-
+          if (needle.Length > 0)
+          {
+            needle = needle.Substring(0, needle.Length - 1);
+          }
         }
+        else if (input.Key == ConsoleKey.Enter)
+        {
+          Console.Clear();
+          Console.Write("Selected Word: ");
+          PrintColor(ConsoleColor.Green, selected_word + '\n');
+          Console.WriteLine("-----------------------------------");
+
+          // find the meaning of the word
+          if (File.Exists(dictionary_file))
+          {
+            StreamReader sr = new StreamReader(dictionary_file);
+            string line = "";
+            bool found = false;
+            while (sr.EndOfStream == false)
+            {
+              line = sr.ReadLine();
+              if (line.ToLower().StartsWith(selected_word.ToLower() + " "))
+              {
+                found = true;
+                if (!String.IsNullOrEmpty(line))
+                {
+                  var parts = line.Split('.');
+                  foreach (var p in parts)
+                  {
+                    Console.WriteLine(p);
+                  }
+                }
+                break;
+              }
+            }
+            if (found == false)
+            {
+              PrintColor(ConsoleColor.Red, "--- Missing Dictionary Entry ---");
+            }
+          }
+          else
+          {
+            PrintColor(ConsoleColor.Red, "--- Missing Dictionary File ---");
+          }
+
+          continue;
+        }
+        else
+        {
+          needle += input.KeyChar;
+        }
+        
+        var regex = new Regex(needle, RegexOptions.IgnoreCase);
+
+        var filtered_words = haystack.Where(r => regex.IsMatch(r)).Take(25);
+        selected_word = filtered_words.OrderBy(x => x.Length).FirstOrDefault();
+        Console.Clear();
+
+        Console.WriteLine($"Search: {needle}");
+        Console.WriteLine("-----------------------------------");
+
+        if (filtered_words.Count() == 0)
+        {
+          PrintColor(ConsoleColor.Red, "--- No Words Found ---");
+        }
+
+        foreach (string fw in filtered_words)
+        {
+          int start_index = fw.IndexOf(needle);
+
+          string before = fw.Substring(0, start_index);
+
+          string after = fw.Substring(start_index + needle.Length);
+
+          Console.Write(before);
+
+          PrintColor(ConsoleColor.Green, needle);
+
+          Console.Write(after + "\n");
+        }
+      }
     }
+
+    private static void PrintColor(ConsoleColor color, string v)
+    {
+      var prevColor = Console.ForegroundColor;
+
+      Console.ForegroundColor = color;
+
+      Console.Write(v);
+
+      Console.ForegroundColor = prevColor;
+    }
+  }
 }
